b5f6424526a26ece1d6e2482c2f5c478c43e9aba
YoelArcos <Yoel.arcoslopez@hotmail.com>
2025-01-21 20:10:27 +0100

    Adds function for retrieving info about the token from the localstorage. Adds simple unauthorized page for the unauthorized users.

---
+++ app/src/util/auth/jwtDecode.js
@@ -0,0 +1,9 @@
+import { jwtDecode } from "jwt-decode";
+
+
+export const getRolesFromToken = () => {
+  const token = localStorage.getItem("jwtToken"); // Retrieve the token from localStorage
+  if (!token) return []; // If no token, return an empty array
+  const decoded = jwtDecode(token); // Decode the JWT
+  return decoded.roles || []; // Return roles or an empty array if roles are not present
+};
