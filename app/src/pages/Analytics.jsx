85e06bb549a8d5e22c72a3b79a2176c38411183d
YoelArcos <Yoel.arcoslopez@hotmail.com>
2025-01-25 10:48:36 +0100

    Adds further permission safe guards.

--- app/src/pages/Analytics.jsx
+++ app/src/pages/Analytics.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { Navigate } from "react-router-dom";
+import { getRolesFromToken } from "../util/auth/jwtDecode";
 
 function Analytics() {
+  const roles = getRolesFromToken();
+  
+  if (!roles.includes("ADMIN")) {
+    return <Navigate to="/unauthorized" />; 
+  }
   return (
     <div>
       <h1>Analytics</h1>
